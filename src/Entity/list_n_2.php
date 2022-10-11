c08658702ff861d8a88e031babdf0d2035e3cb6e
atome5xx <tletoubgym74@gmail.com>
2022-10-11 10:27:15 +0200

    listn_2

--- src/Entity/list_n_2.php
+++ src/Entity/list_n_2.php
@@ -1,6 +1,4 @@
 <?php
-require ('../ConnectionBdd.php');
-$pdo=ConnexionBdd();
 //print n+1
 //$request="SELECT mail_user, name_user, fname_user, id_user FROM `user` WHERE id_job=2;";
 //$req = $pdo->prepare($request);
@@ -10,22 +8,6 @@ $requestr="SELECT region.id_region FROM department, region, user WHERE user.id_d
 $reqr=$pdo->query($requestr);
 while($id_r=$reqr->fetch()){
     $id_r=$id_r['id_region'];
-    //print all id_department from region
-    $requestd="SELECT id_dep FROM department, region WHERE region.id_region='$id_r' AND region.id_region=department.id_region";
-    echo $requestd;
-    $reqd=$pdo->prepare($requestd);
-    if ($reqd->execute())
-        {
-            echo "le nombre de colones retournées est = ".$reqd->rowCount()."<br/>";
-            foreach ($pdo->query($requestd) AS $ligne)
-            {
-                for ($i=0;$i<=$reqd->rowCount()-1;$i++)
-            {
-                echo $ligne[$i];
-            }
-            }
-        }
-        $reqd->closeCursor();
-    }
-var_dump($ligne);
-echo "<br/>".$ligne[0];
+    //print all user information from region
+    $requestd="SELECT department.id_dep, label_dep, mail_user, name_user, fname_user, id_user FROM department, region, user WHERE region.id_region=$id_r AND region.id_region=department.id_region AND department.id_dep=user.id_dep";
+    $reqd=$pdo->prepare($requestd);}
