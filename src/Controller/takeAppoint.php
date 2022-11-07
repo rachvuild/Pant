aad9bf6eded114ba372ba594e8b9a86a4da7854a
atome5xx <tletoubgym74@gmail.com>
2022-11-07 16:32:20 +0100

    up takeappoint

--- src/Controller/takeAppoint.php
+++ src/Controller/takeAppoint.php
@@ -1,8 +1,7 @@
 <?php
+session_start();
+$id_user=$_SESSION["id_user"];
 require('../ConnectionBdd.php');
 $pdo=ConnexionBdd();
-$id_user = $_POST['id_user'];
 require "../../template/Appointment.php";
-$id_user = $_POST['id_user'];
-
 require "../Entity/Appointment.php";
