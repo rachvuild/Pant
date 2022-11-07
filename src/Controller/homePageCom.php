3f2c193c5f9af04d4c0eeb8c103d0dd8cc3055de
rachvuild <62657715+rachvuild@users.noreply.github.com>
2022-11-07 16:34:22 +0100

    pol

---
+++ src/Controller/homePageCom.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../assert/style.css">
+</head>
+
+<body>
+    <div class="PriseRDV">
+
+        <?php
+        require('take');
+        ?>
+    </div>
+    <?php
+    require('');
+    ?>
+    </div>
+    <?php
+    require('');
+    ?>
+    </div>
+
+
+
+
+</body>
+
+</html>
