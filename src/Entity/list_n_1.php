b2fbe1ec10506a7f1ee027db64fa37711b539431
atome5xx <tletoubgym74@gmail.com>
2022-11-15 12:23:13 +0100

    up

--- src/Entity/list_n_1.php
+++ src/Entity/list_n_1.php
@@ -1,10 +1,10 @@
 <?php
 // found department of user
-$dep="SELECT id_dep FROM `user` WHERE id_user='t.letoublon'";
+$dep="SELECT id_dep FROM `user` WHERE id_user='a.dochez'";
 $reponse = $pdo->query($dep);
 while ($department = $reponse->fetch()){
     $d=$department['id_dep'];
 $reponse->closeCursor();}
 // display team of user
-$request="SELECT mail_user, name_user, fname_user, id_user FROM `user` WHERE id_dep=38;";
+$request="SELECT mail_user, name_user, fname_user, id_user FROM `user` WHERE id_dep=$d;";
 $req = $pdo->prepare($request);
--- src/Entity/report_entity.php
+++ src/Entity/report_entity.php
@@ -1,11 +1,25 @@
 <?php
-$id_user='a.dochez';
+if(isset($_POST['compte_rendu'])){
+    $id_user=$_POST['id_user'];
 
-//print necessary information about report that validate
-$inforeport=$pdo->query("SELECT summary_report, interest_report, comments, comments.id_user, date_appoint, report.id_client  FROM `report`, comments, appointment WHERE report.id_report=comments.id_report AND report.id_user='$id_user' AND report.id_appoint=appointment.id_appoint");
+    //print necessary information about report that validate
+    $inforeport=$pdo->query("SELECT summary_report, interest_report, comments, comments.id_user, date_appoint, report.id_client  FROM `report`, comments, appointment WHERE report.id_report=comments.id_report AND report.id_user='$id_user' AND report.id_appoint=appointment.id_appoint");
 
-//print necessary information about report no validate
-$inforeportno=$pdo->query("SELECT DISTINCT summary_report, interest_report, appointment.date_appoint, report.id_client, report.id_report FROM `report`
-LEFT JOIN appointment ON appointment.id_appoint=report.id_appoint
-LEFT JOIN comments USING(id_report) WHERE comments.id_report IS NULL
-AND report.id_user='$id_user'");
+    //print necessary information about report no validate
+    $inforeportno=$pdo->query("SELECT DISTINCT summary_report, interest_report, appointment.date_appoint, report.id_client, report.id_report FROM `report`
+    LEFT JOIN appointment ON appointment.id_appoint=report.id_appoint
+    LEFT JOIN comments USING(id_report) WHERE comments.id_report IS NULL
+    AND report.id_user='$id_user'");
+}
+else{
+    $id_user='a.dochez';
+
+    //print necessary information about report that validate
+    $inforeport=$pdo->query("SELECT summary_report, interest_report, comments, comments.id_user, date_appoint, report.id_client  FROM `report`, comments, appointment WHERE report.id_report=comments.id_report AND report.id_user='$id_user' AND report.id_appoint=appointment.id_appoint");
+
+    //print necessary information about report no validate
+    $inforeportno=$pdo->query("SELECT DISTINCT summary_report, interest_report, appointment.date_appoint, report.id_client, report.id_report FROM `report`
+    LEFT JOIN appointment ON appointment.id_appoint=report.id_appoint
+    LEFT JOIN comments USING(id_report) WHERE comments.id_report IS NULL
+    AND report.id_user='$id_user'");
+}
