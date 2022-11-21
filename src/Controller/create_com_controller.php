561eba8da426bba2f5a1c290554f57eaa57a2c60
atome5xx <tletoubgym74@gmail.com>
2022-11-21 18:12:44 +0100

    session

--- src/Controller/create_com_controller.php
+++ src/Controller/create_com_controller.php
@@ -1,9 +1,19 @@
 <?php
 
 session_start();
-$id_job = $_SESSION["roles_user"];
-$id_user=$_SESSION["id_user"];
-require('../ConnectionBdd.php');
-$pdo=ConnexionBdd();
-require('../Entity/create_com_entity.php');
-require('../../template/create_com_template.php');
+if ($_SESSION == null) {
+    header("location: login.php");
+}
+else{
+    $id_job = $_SESSION["roles_user"];
+    $id_user=$_SESSION["id_user"];
+    if($id_job==2 or $id_job ==3 ){
+        require('../ConnectionBdd.php');
+        $pdo=ConnexionBdd();
+        require('../Entity/create_com_entity.php');
+        require('../../template/create_com_template.php');
+    }
+    else{
+        header("location: homePageCom.php");
+    }
+}
--- src/Entity/list_n_2.php
+++ src/Entity/list_n_2.php
@@ -30,19 +30,3 @@ foreach ($AllDepartement as $ligne) {
     $user->execute();
     $alluser[$id] = $user->fetchAll();
 }
-// var_dump($alluser);
-
-
-// $requestr = "SELECT region.id_region FROM department, region, user WHERE user.id_dep=department.id_dep AND department.id_region=region.id_region AND id_user='t.letoublon'";
-// $reqr = $pdo->query($requestr);
-
-
-
-
-
-// while ($id_r = $reqr->fetch()) {
-//     $id_r = $id_r['id_region'];
-//     //print all user information from region
-//     $requestd = "SELECT department.id_dep, label_dep, mail_user, name_user, fname_user, id_user FROM department, region, user WHERE region.id_region=$id_r AND region.id_region=department.id_region AND department.id_dep=user.id_dep";
-//     $reqd = $pdo->prepare($requestd);
-// }
