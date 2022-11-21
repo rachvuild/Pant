9ec3966e945ec82284e20caa3310f7b3d836bbb9
atome5xx <tletoubgym74@gmail.com>
2022-11-21 15:41:29 +0100

    session homepage

--- template/header.php
+++ template/header.php
@@ -1,11 +1,13 @@
 <html>
-
 <body>
     <div class="header">
         <p><?= $id_user ?></p>
         <?php
-        if ($id_user == 2) {
+        if ($id_job == 2) {
             echo "<a href='list.php'>Mon équipe</a>";
+        }
+        if($id_job == 1 or $id_job == 2){
+            echo "<a href='report_controller.php'>Mes comptes rendu</a>";
         }
         ?>
         <a href="../../template/destroy.php">se deconnecter</a>
