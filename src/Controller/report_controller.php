0024dcb881d9a664ea1f7670a1679d6ea24467ad
atome5xx <tletoubgym74@gmail.com>
2022-11-21 18:08:08 +0100

    session report

--- src/Controller/report_controller.php
+++ src/Controller/report_controller.php
@@ -1,9 +1,14 @@
 <?php
 
 session_start();
-$id_user=$_SESSION['id_user'];
-$id_job = $_SESSION["roles_user"];
-require('../ConnectionBdd.php');
-$pdo=ConnexionBdd();
-require('../Entity/report_entity.php');
-require('../../template/report_template.php');
+if ($_SESSION == null) {
+    header("location: login.php");
+}
+else{
+    $id_user=$_SESSION['id_user'];
+    $id_job = $_SESSION["roles_user"];
+    require('../ConnectionBdd.php');
+    $pdo=ConnexionBdd();
+    require('../Entity/report_entity.php');
+    require('../../template/report_template.php');
+}
