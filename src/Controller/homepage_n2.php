3e72f1a534a19e82e065b4e5d4ab397dfaf5e0d6
atome5xx <tletoubgym74@gmail.com>
2022-11-21 18:22:42 +0100

    upn2

--- src/Controller/homepage_n2.php
+++ src/Controller/homepage_n2.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home_page</title>
-    <link rel="stylesheet" href="../../assert/style.css">
-</head>
 
 <?php
 session_start();
@@ -21,18 +11,9 @@ else{
         include_once('../ConnectionBdd.php');
         $pdo = ConnexionBdd();
         require('../Entity/list_n_2.php');
-        echo '
-        <body class="bodyCom">
-            <div class="header ">';
         require "../../template/header.php";
-        echo '</div>
-        <div>';
         require('../../template/display_n_2.php');
-        echo '</div>
-
-        </body>
-
-        </html>';}
+}
     else{
         echo "<script>alert('Vous n'avez pas les droits');
             document.location.href='homePageCom.php';
