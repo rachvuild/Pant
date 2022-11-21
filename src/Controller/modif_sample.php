b259d32c606983bd68d1609c86c03678a42fa5f4
atome5xx <tletoubgym74@gmail.com>
2022-11-21 17:47:43 +0100

    ok

--- src/Controller/modif_sample.php
+++ src/Controller/modif_sample.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home_page</title>
+    <link rel="stylesheet" href="../../assert/style.css">
+</head>
 <?php
 session_start();
 if ($_SESSION == null) {
