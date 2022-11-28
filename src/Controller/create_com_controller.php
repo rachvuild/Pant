dbb9afa075de612a8404e35f3111e5e28d32f237
B.PAUL <70102915+BillyEllios@users.noreply.github.com>
2022-11-28 17:09:37 +0100

    finish css

--- src/Controller/create_com_controller.php
+++ src/Controller/create_com_controller.php
@@ -8,6 +8,7 @@ if ($_SESSION == null) {
     $id_user = $_SESSION["id_user"];
     if ($id_job == 2 or $id_job == 3) {
         require('../ConnectionBdd.php');
+        require "../../template/header.php";
         require('../Entity/create_com_entity.php');
         require('../../template/create_com_template.php');
     } else {
