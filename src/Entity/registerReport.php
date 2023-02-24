51e663cfc9c32ab62f0a54567711529907c86aab
rachvuild <62657715+rachvuild@users.noreply.github.com>
2023-02-24 10:27:31 +0100

    update

--- src/Entity/registerReport.php
+++ src/Entity/registerReport.php
@@ -24,42 +24,41 @@ if ($registerReport->execute()) {
     $id_report = $donnees['id_report'];
 }
 
-        // Register report in database
-        $registerReport =
-            "INSERT INTO `report`(`summary_report`, `interest_report`, `id_client`, `id_user`, `id_appoint`) 
+// Register report in database
+$registerReport =
+    "INSERT INTO `report`(`summary_report`, `interest_report`, `id_client`, `id_user`, `id_appoint`) 
         VALUES ('$summary','$interest',$id_client,'$id_user',$id_appoint)";
-        $registerReport = $pdo->prepare($registerReport);
-        if ($registerReport->execute()) {
-            $reponse = $pdo->query('SELECT `id_report` FROM `report` ORDER BY `id_report` DESC');
-            $donnees = $reponse->fetch();
-            $id_report = $donnees['id_report'];
-            echo $id_report;
-            $reportinsert=1;
-        }
+$registerReport = $pdo->prepare($registerReport);
+if ($registerReport->execute()) {
+    $reponse = $pdo->query('SELECT `id_report` FROM `report` ORDER BY `id_report` DESC');
+    $donnees = $reponse->fetch();
+    $id_report = $donnees['id_report'];
+    echo $id_report;
+    $reportinsert = 1;
+}
 
 
 
-        //Fetch all sample
-        $sample = "SELECT * FROM `sample`";
-        $sample_req = $pdo->prepare($sample);
-        // Register Sample
-        if ($sample_req->execute()) {
-            foreach ($pdo->query($sample) as $ligne) {
-                $nbSample = $_POST['' . $ligne[1] . ''];
-                echo 'ligne: ' . $ligne[0] . 'id report :' . $id_report . 'nb sample :' . $nbSample . '</br>';
-                if (!empty($nbSample)) {
-                    $nbSample = $_POST['' . $ligne[1] . ''];
-                    $sampleInsert = "INSERT INTO `donner`(`id_sample`, `id_report`, `nb_sample`) VALUES ($ligne[0],$id_report,$nbSample)";
-                    $sampleInsert = $pdo->prepare($sampleInsert);
-                    if($sampleInsert->execute()){
-                        $samplereport=1;
-                    }
-                }
+//Fetch all sample
+$sample = "SELECT * FROM `sample`";
+$sample_req = $pdo->prepare($sample);
+// Register Sample
+if ($sample_req->execute()) {
+    foreach ($pdo->query($sample) as $ligne) {
+        $nbSample = $_POST['' . $ligne[1] . ''];
+        echo 'ligne: ' . $ligne[0] . 'id report :' . $id_report . 'nb sample :' . $nbSample . '</br>';
+        if (!empty($nbSample)) {
+            $nbSample = $_POST['' . $ligne[1] . ''];
+            $sampleInsert = "INSERT INTO `donner`(`id_sample`, `id_report`, `nb_sample`) VALUES ($ligne[0],$id_report,$nbSample)";
+            $sampleInsert = $pdo->prepare($sampleInsert);
+            if ($sampleInsert->execute()) {
+                $samplereport = 1;
             }
         }
-        if($samplereport==1 AND $reportinsert=1){
-            echo "<script>alert('Compte rendu ajouté!');
+    }
+}
+if ($samplereport == 1 and $reportinsert = 1) {
+    echo "<script>alert('Compte rendu ajouté!');
             document.location.href='homePageCom.php';
             </script>";
-        }
-    }*/
+}
