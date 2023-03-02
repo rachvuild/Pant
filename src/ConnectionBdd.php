7324bf56b16926c547bcb2dc5b456f7bcd41324a
rachvuild <62657715+rachvuild@users.noreply.github.com>
2023-03-02 11:55:12 +0100

    maj

--- src/ConnectionBdd.php
+++ src/ConnectionBdd.php
@@ -5,8 +5,3 @@ try {
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
-// echo ("Connection réussi</br>");
-// return $pdo;
-if ($_SESSION == null) {
-    header("location: ../template/connexion.php");
-}
